ab6a48d348bb0cd0c0b165693bbcb72c8f38c8e2
Francisco Hodge <hello@franciscohodge.com>
2018-11-25 17:54:02 -0500

    Update deps

--- build/index.css
+++ build/index.css
@@ -1,6 +1,6 @@
 /*!
  * 
- *   @hodgef/js-library-boilerplate v1.0.5
+ *   @hodgef/js-library-boilerplate v1.0.6
  *   https://github.com/hodgef/js-library-boilerplate
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
