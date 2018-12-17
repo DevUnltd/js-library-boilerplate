f5d92d1d798d77f14da44462d84fdfcede57e530
Francisco Hodge <hello@franciscohodge.com>
2018-12-16 19:06:07 -0500

    Updated devDependencies

--- build/index.css
+++ build/index.css
@@ -1,6 +1,6 @@
 /*!
  * 
- *   @hodgef/js-library-boilerplate v1.0.8
+ *   @hodgef/js-library-boilerplate v1.0.9
  *   https://github.com/hodgef/js-library-boilerplate
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
