5512da46411258e790787bec1592345b0d1cfbaf
Francisco Hodge <hello@franciscohodge.com>
2020-01-13 11:19:26 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   @hodgef/js-library-boilerplate v2.1.75
+ *   @hodgef/js-library-boilerplate v2.1.76
  *   https://github.com/hodgef/js-library-boilerplate
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
