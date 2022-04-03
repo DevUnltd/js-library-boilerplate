daae6502e88e5b7bc23f4801f23d85cd879eb283
Francisco Hodge <hello@franciscohodge.com>
2022-04-02 22:27:10 -0700

    Add globalObject

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -220,6 +220,7 @@ module.exports = function (webpackEnv) {
     // This means they will be the "root" imports that are included in JS bundle.
     entry: paths.appIndexJs,
     output: {
+      globalObject: 'this',
       // The build folder.
       path: paths.appBuild,
       // Add /* filename */ comments to generated require()s in the output.
