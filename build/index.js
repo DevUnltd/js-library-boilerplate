433534b5dfb45832414f98cdda0e3a296cba6f01
Francisco Hodge <hello@franciscohodge.com>
2023-12-04 10:06:44 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   @hodgef/js-library-boilerplate v2.7.262
+ *   @hodgef/js-library-boilerplate v2.7.263
  *   https://github.com/hodgef/js-library-boilerplate
  *
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
@@ -9,5 +9,5 @@
  *   LICENSE file in the root directory of this source tree.
  *
  */
-!function(e,t){"object"===typeof exports&&"object"===typeof module?module.exports=t():"function"===typeof define&&define.amd?define("MyLibrary",[],t):"object"===typeof exports?exports.MyLibrary=t():e.MyLibrary=t()}(this,(function(){return function(){"use strict";var e={d:function(t,r){for(var o in r)e.o(r,o)&&!e.o(t,o)&&Object.defineProperty(t,o,{enumerable:!0,get:r[o]})},o:function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r:function(e){"undefined"!==typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},t={};function _defineProperties(e,t){for(var r=0;r<t.length;r++){var o=t[r];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(e,o.key,o)}}function _createClass(e,t,r){return t&&_defineProperties(e.prototype,t),r&&_defineProperties(e,r),Object.defineProperty(e,"prototype",{writable:!1}),e}e.r(t),e.d(t,{default:function(){return o}});var r=_createClass((function App(){!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,App),this.myVar=!0,this.myArrowMethod=function(){console.log("Arrow method fired")};var e=this.myArrowMethod,t=this.myVar;console.log("Lib constructor called",t),e()})),o=r;return t}()}));
+!function(e,o){"object"===typeof exports&&"object"===typeof module?module.exports=o():"function"===typeof define&&define.amd?define("MyLibrary",[],o):"object"===typeof exports?exports.MyLibrary=o():e.MyLibrary=o()}(this,(()=>(()=>{"use strict";var e={d:(o,t)=>{for(var r in t)e.o(t,r)&&!e.o(o,r)&&Object.defineProperty(o,r,{enumerable:!0,get:t[r]})},o:(e,o)=>Object.prototype.hasOwnProperty.call(e,o),r:e=>{"undefined"!==typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},o={};e.r(o),e.d(o,{default:()=>t});const t=class{constructor(){this.myVar=!0,this.myArrowMethod=()=>{console.log("Arrow method fired")};const{myArrowMethod:e,myVar:o}=this;console.log("Lib constructor called",o),e()}};return o})()));
 //# sourceMappingURL=index.js.map
